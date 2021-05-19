68fcaea9af33b2c56b192b5ccb80b98c5643f394
WasfaNasir04 <wasfanasir4@gmail.com>
2021-05-19 17:26:28 +0500

    phonebook

---
+++ addphonebookController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Phonebook.Controllers
+{
+    public class addphonebookController : Controller
+    {
+        // GET: addphonebook
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
